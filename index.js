6d6bfbf13e1d9ab3832cf956aa716b9659988fa8
Jackson <jackson.njihia@student.moringaschool.com>
2023-12-25 13:12:47 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+
+// 1. basic math functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // 2. basic math functions with increment and decrement
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // 3. makeInt function
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  // 4. preserveDecimal function
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
